9e2bea3c432ee5d1b62c2cd0f102ef277ab349b6
elvess65 <37631917+elvess65@users.noreply.github.com>
2019-01-02 22:36:21 +0200

    UI

---
+++ Clicker/Assets/Scripts/Game/UI/Windows/Craft/UIElement_CraftItem.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace clicker.general.ui
+{
+    public class UIElement_CraftItem : MonoBehaviour
+    {
+        public Text Text_ItemName; 
+
+        public void Init(datatables.DataTableItems.ItemTypes type)
+        {
+
+        }
+    }
+}
