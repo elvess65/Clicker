2a588c221224b653008eb80a1d2c8809679e2439
elvess65 <37631917+elvess65@users.noreply.github.com>
2019-01-03 21:29:42 +0200

    Weapon

---
+++ Clicker/Assets/Scripts/DataTables/DataTableWeapons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static clicker.datatables.DataTableItems;
+
+namespace clicker.datatables
+{
+    public class DataTableWeapons 
+    {
+        #region Data Structures
+        public class Weapon
+        {
+            private ItemTypes m_Type;                           //Тип предмета
+            private int m_Damage;
+            private int m_Durability;
+
+            public ItemTypes Type => m_Type;
+        }
+        #endregion
+    }
+}
