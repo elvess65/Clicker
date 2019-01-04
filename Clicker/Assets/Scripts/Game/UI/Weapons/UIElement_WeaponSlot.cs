954d4246cf3c59aab61848f6d9ec21d0bda4e1b8
elvess65 <37631917+elvess65@users.noreply.github.com>
2019-01-04 19:07:34 +0200

    UI оружия

---
+++ Clicker/Assets/Scripts/Game/UI/Weapons/UIElement_WeaponSlot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace clicker.general.ui
+{
+    public class UIElement_WeaponSlot : MonoBehaviour
+    {
+        public Text Text_ItemName;
+        public Text Text_Amount;
+        public Image Image_DurabilityProgress;
+
+        public void Init(datatables.DataTableItems.ItemTypes type, int amount)
+        {
+            Text_ItemName.text = type.ToString();
+            Text_Amount.text = amount != 0 ? amount.ToString() : string.Empty;
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
