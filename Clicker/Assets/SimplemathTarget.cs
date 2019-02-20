71195f0befabe625309e335718b4096caa7bdc22
elvess65 <37631917+elvess65@users.noreply.github.com>
2019-02-20 18:23:57 +0200

    1

---
+++ Clicker/Assets/SimplemathTarget.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplemathTarget : MonoBehaviour
+{
+    float forceMltp = 1;
+    float friction = 1.5f;
+    Vector3 forceDir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (forceMltp >= 0)
+        {
+            transform.position = transform.position + forceDir * forceMltp * Time.deltaTime;
+            forceMltp -= Time.deltaTime * friction;
+        }
+    }
+
+    public void AddForce(Vector3 force)
+    {
+        forceMltp = 1;
+        //GetComponent<Rigidbody>().AddForce(force);
+        forceDir = force;
+    }
+}
